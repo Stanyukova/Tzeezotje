5110d58c0a72b21cf9859bc137700de80ab5b2d0
Mari <mstanyukova@gmail.com>
2023-02-04 23:20:57 +0600

    fixed request

--- ajax/mail.php
+++ ajax/mail.php
@@ -1,11 +1,11 @@
 <?php
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
+$name = $_GET['name'];
+$phone = $_GET['phone'];
+$message = $_GET['message'];
+$text = "Имя: $name \nТелефон: $phone \nТекст: $message";
 $subject = "=?utf-8?B?".base64_encode('Message from website')."?=";
 $headers = 'From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n';
-$success = mail('mstanyukova@gmail.com', $subject, $message, $headers);
+$success = mail('mstanyukova@gmail.com', $subject, $text, $headers);
 echo $success;
 ?>
